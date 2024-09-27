3c523a77b68581229ab48bb4d3ae02b36841e461
Narr0wB <iliash4@hotmail.com>
2024-09-27 21:24:33 +0200

    New update

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 local base = require("plugins.configs.lspconfig")
-local on_attach = base.on_attach 
-local capabilites = base.capabilities
-
+local on_attach = base.on_attach
+local capabilities = base.capabilities
+local utils = require("lspconfig/util")
 local lspconfig = require("lspconfig")
 
 local servers = {
@@ -18,6 +18,22 @@ local servers = {
     }
   }
 }
+
+lspconfig.glsl_analyzer.setup({})
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"rust"},
+  root_dir = utils.root_pattern("Cargo.toml"),
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      }
+    }
+  }
+})
 
 for _, v in pairs(servers) do
   local name, config = unpack(v)
